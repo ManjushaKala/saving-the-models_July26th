651b91b8f71435f1402265f93d208aba660eadc7
Manjusha Kala <manjusha.kala@gmail.com>
2021-07-28 16:37:38 +0530

    added the model

---
+++ joblib/load_model.py
@@ -0,0 +1,12 @@
+import joblib
+
+#load the model
+
+loaded_model = joblib.load('dib_79.pkl')
+
+pred = loaded_model.predict([[10,20,30,40,50,10,20,10]])
+
+if pred[0] == 1:
+    print('Diabetic')
+else:
+    print('Not Diabetic')
